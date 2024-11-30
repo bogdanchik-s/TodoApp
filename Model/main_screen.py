a8a2ff6d9ebd52d841a9c2b890607e62514d2da4
Bogdan Zakharov <zaharovbogdan2005@yandex.ru>
2024-11-30 11:08:06 +0300

    Main screen: displaying users notes after authorization

--- Model/main_screen.py
+++ Model/main_screen.py
@@ -1,4 +1,5 @@
 from Model.base_model import BaseScreenModel
+from Model.database import Task
 
 
 class MainScreenModel(BaseScreenModel):
@@ -6,3 +7,16 @@ class MainScreenModel(BaseScreenModel):
     Implements the logic of the
     :class:`~View.main_screen.MainScreen.MainScreenView` class.
     """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.tasks: list[Task] = []
+
+    def get_tasks_list(self, owner_id: int) -> list[Task] | None:
+        tasks = self._db.get_data_by_model(model=Task(user_id=owner_id), fetch_all=True)
+
+        if tasks is not None:
+            self.tasks = tasks
+        
+        self.notify_observers('main screen')
